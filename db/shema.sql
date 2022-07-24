eee270861f8239a65a7ffb8553a27fb0b52f557b
braulioloaizac <bloaizac@uqvirtual.edu.co>
2022-07-23 22:40:38 -0400

    first commit

---
+++ db/shema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_traker;
+CREATE DATABASE employee_traker;
+
+USE employee_traker;
+
+CREATE TABLE department(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL 
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  role_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  manager_id INT
+);
